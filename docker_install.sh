70aac0f3aa5ef30216e1114e9a68afdaba017bb5
puchauhan2 <puchauhan4@gmail.com>
2023-08-23 23:38:48 +0530

    added installtion script

---
+++ docker_install.sh
@@ -0,0 +1,6 @@
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl status docker
+sudo usermod -aG docker $(whoami)
+newgrp docker
